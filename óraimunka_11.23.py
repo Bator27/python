acdfbce40c25b6daa3b6e4bc238d92eccf81806c
Dávid Zádori-Fekete <zadorifd@GDO.LAN>
2021-11-29 13:10:23 +0100

    11.29.

---
+++ óraimunka_11.23.py
@@ -0,0 +1,10 @@
+knev = input("Kérem adja meg a kutya nevét!")
+kev = int(input("Kérem adja meg a kutya korát!"))
+kfaj = input("Kérem adja meg a kutya fajtáját!")
+kolt = True
+kutya = {
+    "nev": knev,
+    "faj": kfaj,
+    "otás": kolt
+}
+print(kutya)
